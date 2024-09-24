4482be512cb9d1cf95d84bfbced584d5ed73f5cf
AlexsJones <alexsimonjones@gmail.com>
2024-09-24 14:18:46 +0100

    feat: updated prompt
    Signed-off-by: AlexsJones <alexsimonjones@gmail.com>

---
+++ pkg/prompt/prompt.go
@@ -0,0 +1,6 @@
+package prompt
+
+var (
+	// Prompt is the default prompt for the user
+	Standard = "\"Given the following nodes and analysis of issues in the cluster, I want you to tell me the best node for placement, no other text.\" +\n\t\t\"Please find the data in two segments below: \\n\" +\n\t\t\"1. Nodes in the cluster: %s\\n\" +\n\t\t\"2. Analysis of issues in the cluster (this may be empty) %s\\n\""
+)
