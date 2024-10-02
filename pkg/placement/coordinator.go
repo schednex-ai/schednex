4a40ea7643ffb07fe62932dc21bde0eb90ea7083
AlexsJones <alexsimonjones@gmail.com>
2024-10-02 18:40:19 +0100

    feat: adding relative pods capability
    Signed-off-by: AlexsJones <alexsimonjones@gmail.com>

--- pkg/placement/coordinator.go
+++ pkg/placement/coordinator.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"strings"
+
 	"github.com/go-logr/logr"
 	"github.com/k8sgpt-ai/schednex/pkg/k8sgpt_client"
 	"github.com/k8sgpt-ai/schednex/pkg/prompt"
@@ -12,7 +14,6 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 	metricsv "k8s.io/metrics/pkg/client/clientset/versioned"
-	"strings"
 )
 
 type Coordinator struct {
@@ -33,6 +34,90 @@ func NewCoordinator(client *k8sgpt_client.Client, kubernetesClient *kubernetes.C
 		log:              log.WithName("coordinator"),
 	}
 }
+
+func (c *Coordinator) findNodePlacement(pod v1.Pod) (string, error) {
+	// Find the node the pod is currently placed on
+	nodeName := pod.Spec.NodeName
+	if nodeName != "" {
+		return nodeName, nil
+	}
+	// If the pod is not placed on a node, return an error
+	return "", fmt.Errorf("pod is not placed on a node")
+}
+
+func (c *Coordinator) findRelatives(pod v1.Pod) ([]v1.Pod, error) {
+
+	deduplicate := func(pods []v1.Pod) []v1.Pod {
+		keys := make(map[string]bool)
+		list := []v1.Pod{}
+		for _, pod := range pods {
+			if _, value := keys[pod.Name]; !value {
+				keys[pod.Name] = true
+				list = append(list, pod)
+			}
+		}
+		return list
+	}
+
+	// find if the pod is in a replica set by looking at the owner references
+	for _, r := range pod.OwnerReferences {
+		// Look at all types of controllers...
+		switch r.Kind {
+		case "ReplicaSet":
+			// Get the replica set
+			replicaSet, err := c.kubernetesClient.AppsV1().ReplicaSets(pod.Namespace).Get(context.TODO(), r.Name, metav1.GetOptions{})
+			if err != nil {
+				c.log.Error(err, "Failed to get replica set")
+				return nil, err
+			}
+			// Find out what nodes the other pods are placed on
+			// Get the pods in the replica set
+			pods, err := c.kubernetesClient.CoreV1().Pods(pod.Namespace).List(context.TODO(), metav1.ListOptions{
+				LabelSelector: fmt.Sprintf("pod-template-hash=%s", replicaSet.Labels["pod-template-hash"]),
+			})
+			if err != nil {
+				c.log.Error(err, "Failed to get pods in replica set")
+				return nil, err
+			}
+			return deduplicate(pods.Items), nil
+		case "StatefulSet":
+			// Get the statefulset and other pods
+			statefulSet, err := c.kubernetesClient.AppsV1().StatefulSets(pod.Namespace).Get(context.TODO(), r.Name, metav1.GetOptions{})
+			if err != nil {
+				c.log.Error(err, "Failed to get stateful set")
+				return nil, err
+			}
+			// Find out what nodes the other pods are placed on
+			// Get the pods in the stateful set
+			pods, err := c.kubernetesClient.CoreV1().Pods(pod.Namespace).List(context.TODO(), metav1.ListOptions{
+				LabelSelector: fmt.Sprintf("statefulset.kubernetes.io/pod-name=%s", statefulSet.Name),
+			})
+			if err != nil {
+				c.log.Error(err, "Failed to get pods in stateful set")
+				return nil, err
+			}
+			return deduplicate(pods.Items), nil
+		case "DaemonSet":
+			// Get the daemonset and other pods
+			daemonSet, err := c.kubernetesClient.AppsV1().DaemonSets(pod.Namespace).Get(context.TODO(), r.Name, metav1.GetOptions{})
+			if err != nil {
+				c.log.Error(err, "Failed to get daemon set")
+				return nil, err
+			}
+			// Find out what nodes the other pods are placed on
+			// Get the pods in the daemon set
+			pods, err := c.kubernetesClient.CoreV1().Pods(pod.Namespace).List(context.TODO(), metav1.ListOptions{
+				LabelSelector: fmt.Sprintf("app=%s", daemonSet.Labels["app"]),
+			})
+			if err != nil {
+				c.log.Error(err, "Failed to get pods in daemon set")
+				return nil, err
+			}
+			return deduplicate(pods.Items), nil
+		}
+	}
+	return nil, nil
+}
 func (c *Coordinator) FindNodeForPod(pod v1.Pod, allowAI bool) (string, error) {
 	k8sgptAnalysis, err := c.k8sgptClient.RunAnalysis(allowAI)
 	if err != nil {
@@ -52,9 +137,26 @@ func (c *Coordinator) FindNodeForPod(pod v1.Pod, allowAI bool) (string, error) {
 	if err != nil {
 		return "", err
 	}
+	// get relatives that have a different name to current pod
+	relatives, err := c.findRelatives(pod)
+	if err != nil {
+		c.log.Error(err, "Failed to get relatives")
+	}
+	relative_placement := make(map[string]string)
+	for _, relative := range relatives {
+		if relative.Name != pod.Name {
+			// Find the node the relative is placed on
+			nodeName, err := c.findNodePlacement(relative)
+			if err != nil {
+				continue
+			}
+			relative_placement[fmt.Sprintf("%s is a related pod and resides on", relative.Name)] = nodeName
+		}
+	}
+
 	// Print nodeMetricsListJson
 	// Simple logic: select the first available node (custom logic can go here)
-	combinedPrompt := fmt.Sprintf(prompt.Standard, nodeMetricsListJson, k8sgptAnalysis)
+	combinedPrompt := fmt.Sprintf(prompt.Standard, nodeMetricsListJson, k8sgptAnalysis, relative_placement)
 	// Combine the K8sGPT Analysis and the node metrics to make a decision
 	// Send query
 	response, err := c.k8sgptClient.Query(combinedPrompt)
--- pkg/prompt/prompt.go
+++ pkg/prompt/prompt.go
@@ -2,5 +2,5 @@ package prompt
 
 var (
 	// Prompt is the default prompt for the user
-	Standard = "\"Given the following nodes and analysis of issues in the cluster, I want you to tell me the best node for placement, no other text.\" +\n\t\t\"Please find the data in two segments below: \\n\" +\n\t\t\"1. Nodes in the cluster: %s\\n\" +\n\t\t\"2. Analysis of issues in the cluster (this may be empty) %s\\n\""
+	Standard = "\"Given the following nodes and analysis of issues in the cluster, I want you to tell me the best node for placement, no other text.\" +\n\t\t\"Please find the data in two segments below: \\n\" +\n\t\t\"1. Nodes in the cluster: %s\\n\" +\n\t\t\"2. Analysis of issues in the cluster (this may be empty) %s\\n\" +\n\t\t\"3. Relatives and their current placement: %s\\n\""
 )
